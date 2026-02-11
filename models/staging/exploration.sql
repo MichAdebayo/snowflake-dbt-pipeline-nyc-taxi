5cbf38dfc8ab722afcb9a2bf08182e250a432c2b
Michael Adebayo <ritefluential@gmail.com>
2026-02-11 14:48:25 +0000

    feat: add metrics from taxi data

--- models/staging/exploration.sql
+++ models/staging/exploration.sql
@@ -2,7 +2,7 @@
 
 WITH temp_table AS (
     SELECT 
-        DOLocationID 
+        congestion_surcharge 
     FROM {{ source('raw_nyc_taxi_dataset', 'YELLOW_TAXI_TRIPS') }}
 )
-SELECT count(*) as c_DOLocationID  from temp_table where DOLocationID  is null
+SELECT count(*) as tot from temp_table where congestion_surcharge < 0
